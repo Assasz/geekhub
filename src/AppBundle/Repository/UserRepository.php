3af38586916258693c89aeb12267ab12e78dc15d
Assasz <pawel.assasz678@gmail.com>
2017-10-23 20:43:24 +0200

    add new user sorting

--- src/AppBundle/Repository/UserRepository.php
+++ src/AppBundle/Repository/UserRepository.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\Query\ResultSetMapping;
 
 /**
  * UserRepository
@@ -12,12 +13,25 @@ class UserRepository extends EntityRepository
     //query for users matching the given input
     public function searchForUsersQuery($input, $sort)
     {
-        return $query = $this->createQueryBuilder('u')
+        $query = $this->createQueryBuilder('u')
             ->Where('u.username LIKE :input')
             ->orWhere('u.surname LIKE :input')
             ->orWhere('u.forename LIKE :input')
             ->setParameter('input', '%'.$input.'%')
-            ->orderBy('u.'.$sort, 'DESC')
-            ->getQuery();
+            ->leftJoin('u.posts', 'p')
+            ->leftJoin('u.followers', 'f')
+            ->select('u, count(p.id) as posts, count(f.id) as followers')
+            ->groupBy('u.id');
+
+        if($sort == 'createDate')
+        {
+            $query->orderBy('u.createDate', 'ASC');
+        }
+        else
+        {
+            $query->orderBy($sort, 'DESC');
+        }
+
+        return $query->getQuery();
     }
 }
